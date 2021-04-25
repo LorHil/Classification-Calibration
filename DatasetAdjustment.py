44d229cdae6063f5290c6d1a14e0a96e608e848b
LorHil <73492424+LorHil@users.noreply.github.com>
2021-04-25 17:04:16 +0200

    Update DatasetAdjustment.py

--- DatasetAdjustment.py
+++ DatasetAdjustment.py
@@ -1,7 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import cm
-#import pandas as pd
 import os, sys
 
 
@@ -23,11 +20,31 @@ if __name__ == '__main__':
                 csvfile.write(',')
 
                 tekst = txtfile.read()
-                tekst = tekst.replace('\n',' ') #something is still wrong, too many columns!
+                tekst = tekst.replace('\n',' ') 
                 tekst = tekst.replace('"',"\'")
                 
                 csvfile.write('\"')
                 csvfile.write(tekst)
                 csvfile.write('\"')
                 csvfile.write('\n')
+            txtfile.close()
+        csvfile.close()
 
+    files2 = os.listdir("C:\\Users\\loreh\\Downloads\\Test")
+    with open('test.csv', 'w') as csvfile:
+        # given titles to the columns
+        csvfile.write('EmailText')
+        csvfile.write('\n')
+
+        for f in files2:
+            with open("C:\\Users\\loreh\\Downloads\\Test\\{}".format(f),'r') as txtfile:  
+                tekst = txtfile.read()
+                tekst = tekst.replace('\n',' ')
+                tekst = tekst.replace('"',"\'")
+                
+                csvfile.write('\"')
+                csvfile.write(tekst)
+                csvfile.write('\"')
+                csvfile.write('\n')
+            txtfile.close()
+        csvfile.close()
